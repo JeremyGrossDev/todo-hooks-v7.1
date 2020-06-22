9c8effcda04e4586e967250f206430500ae4da0b
Jeremy Gross <31456090+JeremyGrossMT@users.noreply.github.com>
2020-06-22 06:25:36 -0600

    Added date to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,39 @@ import React from "react";
 import "./Header.scss";
 
 const Header = () => {
-  return <div className="header">Machen</div>;
+  let date = new Date();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const days = ["Sun", "Monday", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  const displayDate =
+    days[date.getDay()] +
+    " " +
+    months[date.getMonth()] +
+    " " +
+    date.getUTCDate() +
+    " " +
+    date.getUTCFullYear();
+  return (
+    <div className="header">
+      <div>Machen</div>
+      <div className="header-date">{displayDate}</div>
+    </div>
+  );
 };
 
 export default Header;
