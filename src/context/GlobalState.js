c3349caa1b9c5793d00b11d3c4d564a4f80e41af
Jeremy Gross <31456090+JeremyGrossMT@users.noreply.github.com>
2020-06-30 20:30:18 -0600

    Cleaned up GlobalState.js

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -13,13 +13,12 @@ const configsForState = JSON.parse(localStorage.getItem("configs")) || [
   },
 ];
 
-//const isSortTaskManual = configsForState[0].configsForState;
-
-//const sortedTasksByOrder = tasksForState.sort((a, b) => a.order - b.order);
-
-/* const sortedTasks = tasksForState.sort(
+const isSortTaskManual = configsForState[0].sortTaskManual;
+const sortedTasks = tasksForState.sort(
   (a, b) => a.priority - b.priority || a.title.localeCompare(b.title)
-); */
+);
+const isSorted = isSortTaskManual ? sortedTasks : tasksForState;
+console.log(isSorted);
 
 /* const orderdTasks = sortedTasks.map((task, i) => {
   return {
@@ -32,102 +31,20 @@ const configsForState = JSON.parse(localStorage.getItem("configs")) || [
   };
 }); */
 
-//const tasks = isSortTaskManual ? orderdTasks : tasksForState;
-
-//console.log(sortedTasksByOrder);
-//console.log(tasks);
-
-/* const sortedTasks = tasksForState.sort(
-  (a, b) => a.priority - b.priority || a.title.localeCompare(b.title)
-);
-
-const orderdTasks = sortedTasks.map((task, i) => {
-  return {
-    order: i,
-    id: task.id,
-    title: task.title,
-    priority: task.priority,
-    today: task.today,
-    isComplete: task.isComplete,
-  };
-}); */
-
 const initialState = {
-  tasks: tasksForState,
+  tasks: isSorted,
   configs: configsForState,
 };
-/* const initialState = {
-  tasks: [
-    {
-      id: 1,
-      title: "Feed the cat to the dog",
-      priority: 2,
-      today: true,
-      isComplete: false,
-    },
-    {
-      id: 2,
-      title: "Give the fish a bath",
-      priority: 1,
-      today: false,
-      isComplete: false,
-    },
-    {
-      id: 3,
-      title: "Watch a snells for a race",
-      priority: 3,
-      today: true,
-      isComplete: true,
-    },
-  ],
-  configs: [
-    {
-      showTaskInfo: false,
-      showTodays: false,
-      editTaskInfo: null,
-    },
-  ],
-}; */
 
 export const GlobalContext = createContext(initialState);
 
 export const GlobalContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(logger(AppReducer), initialState);
-  //console.log(state);
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(state.tasks));
     localStorage.setItem("configs", JSON.stringify(state.configs));
   });
-
-  /* console.log(state.tasks);
-  const sortedTasks = state.tasks.sort(
-    (a, b) => a.priority - b.priority || a.title.localeCompare(b.title)
-  );
-
-  const orderdTasks = sortedTasks.map((task, i) => {
-    return {
-      order: i,
-      id: task.id,
-      title: task.title,
-      priority: task.priority,
-      today: task.today,
-      isComplete: task.isComplete,
-    };
-  }); */
-
-  /* const relocateIndex = (array, index, delta) => {
-    console.log("move", array, index, delta);
-
-    var newIndex = index + delta;
-    if (newIndex < 0 || newIndex === array.length) return; //Already at the top or bottom.
-    var indexes = [index, newIndex].sort((a, b) => a - b); //Sort the indixes (fixed)
-    array.splice(indexes[0], 2, array[indexes[1]], array[indexes[0]]); //Replace from lowest index, two elements, reverting the order
-
-    return array;
-  }; */
-
-  //console.log(relocateIndex(orderdTasks, 3, -1));
 
   const addTask = (task) => {
     dispatch({
